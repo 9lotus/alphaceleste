61b060205e95fc427982e2531536dc163eead48c
Zenia Haroon <93739116+zeniaharoon@users.noreply.github.com>
2023-08-09 16:47:04 -0400

    Draft 1 of ML

---
+++ gymEnvironment.py
@@ -0,0 +1,52 @@
+import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
+from celeste import CelesteEnvironment
+import math as mth
+
+level_endpos = (312,69)
+level_startpos = (16, 156)
+screen_height = 180
+screen_width = 320
+
+class CelesteGymEnv(gym.Env):
+    def __init__(self):
+        self.env = CelesteEnvironment()
+        self.action_space = gym.spaces.Discrete(8)  # Actions: 0 to 7
+        self.observation_space = spaces.Dict(
+            {
+                'maddy_x': spaces.Box(low=0, high=screen_width, shape=(1,), dtype = np.uint8),
+                'maddy_y': spaces.Box(low=0, high=screen_height, shape=(1,), dtype = np.uint8),
+                'maddy_x_velocity': spaces.Box(low=0, high=1.5, shape=(1,), dtype = float),
+                'maddy_y_velocity': spaces.Box(low= 0, high=3.4, shape=(1,), dtype = float),
+                'dist2goal': spaces.Box(low=0, high=(mth.sqrt((self.screen_width **2)+ (self.screen_height **2))), shape = (1,), dtype = float)
+            }
+        )
+        self.done = False
+        self.startpos = level_startpos
+        self.endpos = level_endpos
+
+    def step(self, action):
+        obs, _, done, _ = self.env.step(action)
+        agent_pos = self.env.maddy_pos
+        distance_to_end = mth.sqrt((self.endpos[0] - agent_pos[0])**2 + (self.endpos[1] - agent_pos[1])**2)
+        reward = 1.0 / (1.0 + distance_to_end)  # Higher reward for being closer to endpos
+
+        self.done = self.env.maddy_pos[0] >= self.endpos[0]  # Check if the agent has reached the endpos
+
+        return obs, reward, done, {}
+
+    def reset(self):
+        obs = self.env.reset()
+        self.done = False
+        return obs
+
+    def render(self, mode='human'):
+        self.env.render()
+
+    def close(self):
+        self.env.close()
+
+# Create the Gym environment
+env = CelesteGymEnv()
+
--- main.py
+++ main.py
@@ -5,13 +5,38 @@ Date : 7/27/23 - TBD
 
 Description: Runs the Celeste program
 """
+import stable_baselines3 as sb3
+from stable_baselines3.common.env_checker import check_env
 
 from celeste import CelesteEnvironment
 
 Celeste = CelesteEnvironment()
+
+# if human playing, this loop
 terminated = False
 while not terminated:
     action = Celeste.get_playerinput()
     terminated = Celeste.step(action)
     Celeste.render()
+
+'''
+check_env(Celeste)
+ # Set up model
+model = sb3.PPO("MultiInputPolicy", Celeste, verbose=1)
+model.learn(total_timesteps=250000)
+model.save("alphaceleste")
+
+del model
+#Load and evaluate agent
+model = sb3.PPO.load("alphaceleste")
+obs = Celeste.reset()
+
+# if ai playing, this loop
+done = False
+while not done:
+    action, _ = model.predict(obs)
+    obs, reward, done, info = Celeste.step(action)
+    Celeste.render()
+
+'''
 Celeste.close()
